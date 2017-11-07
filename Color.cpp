7b702efe5a4f3138800a12d71de160d770466055
Grace <goldflutist@gmail.com>
2017-11-06 23:24:37 -0500

    LOL
    LOLLL

---
+++ Color.cpp
@@ -0,0 +1,103 @@
+/**
+ * Color.cpp
+ *
+ * EECS 183, Fall 2017
+ * Project 4: CoolPics
+ *
+ * <#Name(s)#>
+ * <#uniqname(s)#>
+ *
+ * <#Description#>
+ */
+
+#include "Color.h"
+
+// TODO: implement first checkRange, then two constructors, setRed, getRed,
+//       setGreen, getGreen, setBlue, getBlue, read, write.
+Color::Color(){
+    red = 0;
+    green = 0;
+    blue = 0;
+    
+    return;
+}
+
+Color::Color(int redVal, int greenVal, int blueVal){
+    red = checkRange(redVal);
+    green = checkRange(greenVal);
+    blue = checkRange(blueVal);
+    return;
+}
+
+void Color::setRed(int redVal){
+    red = checkRange(redVal);
+    
+    return;
+}
+
+int Color::getRed(){
+    return red;
+}
+
+void Color::setGreen(int greenVal){
+    green = checkRange(greenVal);
+    
+    return;
+}
+
+int Color::getGreen(){
+    return green;
+}
+
+void Color::setBlue(int blueVal){
+    blue = checkRange(blueVal);
+    
+    return;
+}
+
+int Color::getBlue(){
+    return blue;
+}
+
+void Color::read(istream& ins){
+    ins >> red >> green >> blue;
+    
+    setRed(red);
+    setGreen(green);
+    setBlue(blue);
+    
+    return;
+}
+
+void Color::write(ostream &outs){
+    outs << red << green << blue;
+    return;
+}
+
+int Color::checkRange(int val) {
+    if (val >= 0 && val <= 255){
+        return val;
+    }
+    else if (val < 0){
+        return 0;
+    }
+    else {
+        return 255;
+    }
+}
+
+
+// Your code goes above this line.
+// Don't change the implementations below!
+
+istream& operator >> (istream& ins, Color& color)
+{
+   color.read(ins);
+   return ins;
+}
+
+ostream& operator << (ostream& outs, Color color)
+{
+   color.write(outs);
+   return outs;
+}
---
+++ Point.cpp
@@ -0,0 +1,106 @@
+/**
+ * Point.cpp
+ *
+ * EECS 183, Fall 2017
+ * Project 4: CoolPics
+ *
+ * <#Name(s)#>
+ * <#uniqname(s)#>
+ *
+ * <#Description#>
+ */
+
+#include "Point.h"
+
+// for the declaration of DIMENSION
+#include "utility.h"
+
+// TODO: implement two constructors, setX, getX, setY, getY, read, write, checkRange.
+
+Point::Point() {
+    x = 0;
+    y = 0;
+    
+    return;
+    
+}
+
+Point::Point(int xVal, int yVal) {
+    
+    x = checkRange(xVal);
+    y = checkRange(yVal);
+    
+    return;
+}
+
+void Point::setX(int xVal) {
+    x = checkRange(xVal);
+    
+    return;
+}
+
+int Point::getX() {
+    
+    return x;
+}
+
+void Point::setY(int yVal) {
+
+    y = checkRange(yVal);
+    
+    return;
+}
+
+int Point::getY() {
+    
+    return y;
+}
+
+void Point::read(istream& ins) {
+    
+    char trash;
+    
+    ins >> trash >> x >> trash >> y >> trash;
+    
+    setX(x);
+    setY(y);
+    
+    
+    return;
+}
+
+void Point::write(ostream& outs) {
+    
+    outs << "(" << x << "," << y << ")";
+    return;
+}
+
+int Point::checkRange(int val) {
+    if (val >= 0 && val < DIMENSION){
+        return val;
+    }
+    else if (val < 0){
+        return 0;
+    }
+    else {
+        return DIMENSION - 1;
+    }
+}
+
+
+
+
+// Your code goes above this line.
+// Don't change the implementations below!
+
+istream& operator >> (istream& ins,  Point& pt)
+{
+    pt.read(ins);
+    return ins;
+}
+
+ostream& operator<< (ostream& outs, Point pt)
+{
+    pt.write(outs);
+    return outs;
+}
